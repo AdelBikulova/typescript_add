bfb6a15092de0ddec6e5060c6a5d1de32c853409
AIT TR Student <arbikulova@gmail.com>
2024-04-23 16:53:36 +0200

    Lesson06

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css';
 
+// Lessons
+import Lesson06 from './lessons/Lessons06/Lessons06';
+
+// Homeworks
+
+// Consultations
+
+
 function App() {
-  return <div className="App">My React-TS project</div>  
+  return (
+    <div className="App">
+      <Lesson06 />
+    </div>
+  );
 }
 
 export default App;
