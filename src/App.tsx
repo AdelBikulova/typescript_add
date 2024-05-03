44d0e424afddf8313a3dec85197894dbffc44ea9
AIT TR Student <arbikulova@gmail.com>
2024-05-03 17:04:27 +0200

    Lesson13

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import GlobalStyles from "styles/GlobalStyles";
-import Layout from "components/Layout/Layout";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GlobalStyles from 'styles/GlobalStyles';
+import Layout from 'components/Layout/Layout';
+import About from 'pages/About/About';
+import Home from 'pages/Home/Home';
+import Users from 'pages/Users/Users';
 
 // Lessons
 // import Lesson06 from 'lessons/Lesson06/Lesson06';
@@ -10,6 +15,7 @@ import Layout from "components/Layout/Layout";
 // import Lesson11 from 'lessons/Lesson11/Lesson11';
 // import Lesson12 from 'lessons/Lesson12/Lesson12';
 
+
 // Homeworks
 // import Homework07 from 'homeworks/Homework07/Homework07'
 // import Homework08 from 'homeworks/Homework08/Homework08';
@@ -18,13 +24,21 @@ import Layout from "components/Layout/Layout";
 
 // Consultations
 // import Consultation04 from 'consultations/Consultation04/Consultation04';
-import Consultation05 from "consultation/Consultation05/Consultation05";
+// import Consultation05 from 'consultations/Consultation05/Consultation05';
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
-      <Layout />
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='*' element='Page Not Found'/>
+        </Routes>
+      </Layout>
       {/* <Lesson06 /> */}
       {/* <Lesson07 /> */}
       {/* <Homework07 /> */}
@@ -37,8 +51,8 @@ function App() {
       {/* <Lesson11 /> */}
       {/* <Homework11 /> */}
       {/* <Lesson12 /> */}
-      <Consultation05 />
-    </>
+      {/* <Consultation05 /> */}
+    </BrowserRouter>
   );
 }
 
